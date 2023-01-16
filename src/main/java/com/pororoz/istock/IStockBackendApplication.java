cb4a89325221e87ab73e45f98bad685acfb89eea
dohyeon-han <limited.hyeon@gmail.com>
2023-01-16 23:49:10 +0900

    initialize spring boot

---
+++ src/main/java/com/pororoz/istock/IStockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pororoz.istock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IStockBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IStockBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pororoz/istock/IStockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pororoz.istock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IStockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
