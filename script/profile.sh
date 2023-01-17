eb7b5cabb913e914f9ea20bff42a81598d95bc36
dohyeon-han <limited.hyeon@gmail.com>
2023-01-18 02:36:41 +0900

    fix: nginx 스크립트 수정

--- script/profile.sh
+++ script/profile.sh
@@ -2,13 +2,13 @@
 
 # nginx와 연결되지 않은 profile 찾기
 function find_idle_profile() {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/api/profile)
 
   if [ "$RESPONSE_CODE" -ge 400 ]
   then
     CURRENT_PROFILE=spring2
   else
-    CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    CURRENT_PROFILE=$(curl -s http://localhost/api/profile)
   fi
 
   # nginx와 연결되지 않은 profile 설정
