dcd154c69a7041d37d40dcc234e5474017e6cb61
pythonstrup <pythonstrup@gmail.com>
2023-01-18 15:18:19 +0900

    feat: ExceptionStatus 작성

---
+++ src/main/java/com/pororoz/istock/common/utils/message/ExceptionStatus.java
@@ -0,0 +1,15 @@
+package com.pororoz.istock.common.utils.message;
+
+public class ExceptionStatus {
+
+    // Controlled Range
+    public static final String BAD_REQUEST = "BAD_REQUEST";
+    public static final String PAGE_NOT_FOUND = "PAGE_NOT_FOUND";
+
+    // Uncontrolled Range
+    public static final  String RUNTIME_ERROR = "RUNTIME_ERROR";
+    public static final  String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+
+
+    private ExceptionStatus() {}
+}
