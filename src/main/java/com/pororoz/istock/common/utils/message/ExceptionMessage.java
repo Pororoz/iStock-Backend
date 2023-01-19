6275368907a39d805bfe72d06bb57ab2b8b092c8
pythonstrup <pythonstrup@gmail.com>
2023-01-19 17:52:23 +0900

    feat: RoleNotFoundException 작성
    Co-authored-by: dohyeon-han <limited.hyeon@gmail.com>

--- src/main/java/com/pororoz/istock/common/utils/message/ExceptionMessage.java
+++ src/main/java/com/pororoz/istock/common/utils/message/ExceptionMessage.java
@@ -4,5 +4,9 @@ public class ExceptionMessage {
 
     public static final String PAGE_NOT_FOUND = "해당 페이지는 존재하지 않습니다.";
 
+    public static final String ROLE_NOT_FOUND = "해당 Role이 존재하지 않습니다.";
+
+    public static final String INVALID_PASSWORD = "형식에 맞지 않는 비밀번호입니다.";
+
     private ExceptionMessage() {}
 }
--- src/main/java/com/pororoz/istock/common/utils/message/ExceptionStatus.java
+++ src/main/java/com/pororoz/istock/common/utils/message/ExceptionStatus.java
@@ -5,6 +5,7 @@ public class ExceptionStatus {
     // Controlled Range
     public static final String BAD_REQUEST = "BAD_REQUEST";
     public static final String PAGE_NOT_FOUND = "PAGE_NOT_FOUND";
+    public static final String ROLE_NOT_FOUND = "ROLE_NOT_FOUND";
 
     // Uncontrolled Range
     public static final  String RUNTIME_ERROR = "RUNTIME_ERROR";
