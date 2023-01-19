c091684cfe09610595077bd520e3878ac87fc29c
pythonstrup <pythonstrup@gmail.com>
2023-01-19 17:52:40 +0900

    chore: User username Size 수정
    Co-authored-by: dohyeon-han <limited.hyeon@gmail.com>

--- src/main/java/com/pororoz/istock/domain/user/entity/User.java
+++ src/main/java/com/pororoz/istock/domain/user/entity/User.java
@@ -22,7 +22,7 @@ public class User implements Serializable {
     private Long id;
 
     @NotNull
-    @Size(min = 1, max = 20)
+    @Size(min = 4, max = 20)
     @Column(length = 50, unique = true, nullable = false)
     private String username;
 
