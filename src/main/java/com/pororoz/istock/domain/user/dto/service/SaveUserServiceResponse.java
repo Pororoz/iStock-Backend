0bdddfccd928d8802f0e2e857b1fc3a241c517fb
pythonstrup <pythonstrup@gmail.com>
2023-01-19 15:27:23 +0900

    feat: SaveUserServiceResponse 작성
    Co-authored-by: dohyeon-han <limited.hyeon@gmail.com>

---
+++ src/main/java/com/pororoz/istock/domain/user/dto/service/SaveUserServiceResponse.java
@@ -0,0 +1,30 @@
+package com.pororoz.istock.domain.user.dto.service;
+
+import com.pororoz.istock.domain.user.entity.Role;
+import com.pororoz.istock.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SaveUserServiceResponse {
+    private Long id;
+    private String username;
+    private Role role;
+
+    @Override
+    public boolean equals(Object obj) {
+        SaveUserServiceResponse response = (SaveUserServiceResponse) obj;
+        return id.equals(response.getId()) &&
+                username.equals(response.getUsername()) &&
+                role.getName().equals(response.getRole().getName());
+    }
+
+    public static SaveUserServiceResponse of(User user) {
+        return SaveUserServiceResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .role(user.getRole())
+                .build();
+    }
+}
