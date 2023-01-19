c447fed8681ad8bc47a28d710aea143308c2e8ad
pythonstrup <pythonstrup@gmail.com>
2023-01-19 15:22:45 +0900

    feat: User & Role Entity 수정
    Co-authored-by: dohyeon-han <limited.hyeon@gmail.com>

--- src/main/java/com/pororoz/istock/domain/user/entity/User.java
+++ src/main/java/com/pororoz/istock/domain/user/entity/User.java
@@ -9,7 +9,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Collection;
 
 @Builder
 @AllArgsConstructor
@@ -32,11 +31,7 @@ public class User implements Serializable {
     @Column(nullable = false)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
-    )
-    private Collection<Role> roles;
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Role role;
 }
