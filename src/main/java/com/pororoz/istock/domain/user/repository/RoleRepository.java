c3c9a47cfc0ee68f92d531205ad9ec22a4375f09
pythonstrup <pythonstrup@gmail.com>
2023-01-19 17:43:02 +0900

    feat: RoleRepository findByName 작성
    Co-authored-by: dohyeon-han <limited.hyeon@gmail.com>

--- src/main/java/com/pororoz/istock/domain/user/repository/RoleRepository.java
+++ src/main/java/com/pororoz/istock/domain/user/repository/RoleRepository.java
@@ -3,5 +3,8 @@ package com.pororoz.istock.domain.user.repository;
 import com.pororoz.istock.domain.user.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
