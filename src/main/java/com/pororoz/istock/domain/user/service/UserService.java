e37d0f49b51ad29ff89caa77cc10ea092a54bb7b
pythonstrup <pythonstrup@gmail.com>
2023-01-19 17:45:31 +0900

    feat: UserService 작성
    Co-authored-by: dohyeon-han <limited.hyeon@gmail.com>

--- src/main/java/com/pororoz/istock/domain/user/service/UserService.java
+++ src/main/java/com/pororoz/istock/domain/user/service/UserService.java
@@ -3,6 +3,8 @@ package com.pororoz.istock.domain.user.service;
 import com.pororoz.istock.domain.user.dto.service.SaveUserServiceRequest;
 import com.pororoz.istock.domain.user.dto.service.SaveUserServiceResponse;
 import com.pororoz.istock.domain.user.entity.User;
+import com.pororoz.istock.domain.user.exception.RoleNotFoundException;
+import com.pororoz.istock.domain.user.repository.RoleRepository;
 import com.pororoz.istock.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     public SaveUserServiceResponse saveUser(SaveUserServiceRequest saveUserServiceRequest) {
-        User user = saveUserServiceRequest.toUser();
+        Role role = roleRepository.findByName(saveUserServiceRequest.getRoleName())
+                .orElseThrow(RoleNotFoundException::new);
+        User user = saveUserServiceRequest.toUser(role);
         User result = userRepository.save(user);
         return SaveUserServiceResponse.of(result);
     }
