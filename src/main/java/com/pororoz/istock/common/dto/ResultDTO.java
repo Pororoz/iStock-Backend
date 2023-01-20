39a1d058a9616ded6a03b2e10d87c10d9f0eabf6
pythonstrup <pythonstrup@gmail.com>
2023-01-20 15:08:26 +0900

    feat: saveUser Swagger 설정
    Co-authored-by: dohyeon-han <limited.hyeon@gmail.com>

--- src/main/java/com/pororoz/istock/common/dto/ResultDTO.java
+++ src/main/java/com/pororoz/istock/common/dto/ResultDTO.java
@@ -1,6 +1,5 @@
 package com.pororoz.istock.common.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -10,11 +9,9 @@ import lombok.Data;
 @Data
 public class ResultDTO<D> {
 
-    @Schema(description = "Result Code", example = "OK")
     @NotBlank
     private final String status;
 
-    @Schema(description = "Message", example = "Success")
     @NotBlank
     private final String message;
 
--- src/main/java/com/pororoz/istock/domain/user/dto/response/SaveUserResponse.java
+++ src/main/java/com/pororoz/istock/domain/user/dto/response/SaveUserResponse.java
@@ -1,5 +1,6 @@
 package com.pororoz.istock.domain.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,10 +8,13 @@ import lombok.Getter;
 @Builder
 public class SaveUserResponse {
 
+    @Schema(description = "사용자 아이디", example = "1")
     private Long id;
 
+    @Schema(description = "사용자 이름", example = "pororoz")
     private String username;
 
+    @Schema(description = "역할", example = "user")
     private String roleName;
 
     @Override
