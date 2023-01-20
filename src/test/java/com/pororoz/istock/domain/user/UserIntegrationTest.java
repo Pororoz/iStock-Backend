9670440726103f554e4b1217980d2be362818c93
pythonstrup <pythonstrup@gmail.com>
2023-01-20 16:51:36 +0900

    feat: User API 통합테스트 deleteUser 성공케이스

--- src/test/java/com/pororoz/istock/domain/user/UserIntegrationTest.java
+++ src/test/java/com/pororoz/istock/domain/user/UserIntegrationTest.java
@@ -7,6 +7,9 @@ import com.pororoz.istock.common.utils.message.ExceptionStatus;
 import com.pororoz.istock.common.utils.message.ResponseMessage;
 import com.pororoz.istock.common.utils.message.ResponseStatus;
 import com.pororoz.istock.domain.user.dto.request.SaveUserRequest;
+import com.pororoz.istock.domain.user.entity.Role;
+import com.pororoz.istock.domain.user.entity.User;
+import com.pororoz.istock.domain.user.exception.RoleNotFoundException;
 import com.pororoz.istock.domain.user.repository.RoleRepository;
 import com.pororoz.istock.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.*;
@@ -17,7 +20,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -40,6 +46,77 @@ public class UserIntegrationTest {
 
     @Autowired
     DatabaseCleanup databaseCleanup;
+
+    @Nested
+    @DisplayName("DELETE /users/{id} 계정 삭제 API")
+    @Transactional
+    class DeleteUser {
+        private String url(long id) {
+            return "/v1/users" + "/" + id;
+        }
+
+        MultiValueMap<String, String> params;
+
+        private Long id;
+        private String username;
+        private String password;
+        private String roleName;
+
+        @BeforeEach
+        public void beforeEach() {
+            id = 1L;
+            username = "test";
+            password = "1234a";
+            roleName = "user";
+            params = new LinkedMultiValueMap<>();
+        }
+
+        @AfterEach
+        public void afterEach() {
+            databaseCleanup.execute();
+        }
+
+        @Nested
+        @DisplayName("성공 케이스")
+        class SuccessCase {
+            @Test
+            @DisplayName("존재하는 유저를 삭제하면 삭제에 성공한다.")
+            void deleteUser() throws Exception{
+                // given
+                Role role = roleRepository.findByName(roleName).orElseThrow(RoleNotFoundException::new);
+                User user = User.builder().username(username).password(password).role(role).build();
+                userRepository.save(user);
+
+                // when
+                ResultActions actions = mockMvc.perform(delete(url(id))
+                        .contentType(MediaType.APPLICATION_JSON));
+
+                // then
+                actions.andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("$.status").value(ResponseStatus.OK))
+                        .andExpect(jsonPath("$.message").value(ResponseMessage.DELETE_USER))
+                        .andExpect(jsonPath("$.data.id").value(id))
+                        .andExpect(jsonPath("$.data.username").value(username))
+                        .andExpect(jsonPath("$.data.roleName").value(roleName))
+                        .andDo(print());
+            }
+        }
+
+        @Nested
+        @DisplayName("실패 케이스")
+        class FailCase {
+//            @Test
+//            @DisplayName("존재하지 않는 유저를 삭제하면 404 Error와 USER_NOT_FOUND error를 반환받는다.")
+//            void userNotFound() throws Exception {
+//                //given
+//
+//                //when
+//
+//                //then
+//            }
+        }
+    }
 
     @Nested
     @DisplayName("POST /users 계정 생성 API")
