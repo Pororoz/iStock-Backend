eee6c17f3e40f61a7dce9efc0d3666be05d74060
pythonstrup <pythonstrup@gmail.com>
2023-01-21 14:20:06 +0900

    feat: UpdateUserServiceRequest 작성

---
+++ src/main/java/com/pororoz/istock/domain/user/dto/service/UpdateUserServiceRequest.java
@@ -0,0 +1,12 @@
+package com.pororoz.istock.domain.user.dto.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UpdateUserServiceRequest {
+    private Long id;
+    private String password;
+    private String roleName;
+}
