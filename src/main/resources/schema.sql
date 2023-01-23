7f3fd40326171f2b0e30e009f81cd2f0df9dc9a6
dohyeon-han <limited.hyeon@gmail.com>
2023-01-23 19:45:04 +0900

    fix: integration test 오류 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 INSERT INTO role (id, name)
 SELECT * FROM (SELECT 1, "admin") AS tmp
-WHERE NOT EXISTS(SELECT * FROM ROLE WHERE name="admin");
+WHERE NOT EXISTS(SELECT * FROM role WHERE name="admin");
 
 INSERT INTO role (id, name)
 SELECT * FROM (SELECT 2, "user") AS tmp
-WHERE NOT EXISTS(SELECT * FROM ROLE WHERE name="user");
+WHERE NOT EXISTS(SELECT * FROM role WHERE name="user");
