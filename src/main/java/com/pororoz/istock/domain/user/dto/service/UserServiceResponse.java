7addaa0ab70849c1c7bae5243e6c74d914515ad4
dohyeon-han <limited.hyeon@gmail.com>
2023-01-24 15:23:57 +0900

    feat: UserServiceResponse에 createdAt, updatedAt 추가
    이에 따라 equals, hashCode 수정

--- src/main/java/com/pororoz/istock/domain/user/dto/service/UserServiceResponse.java
+++ src/main/java/com/pororoz/istock/domain/user/dto/service/UserServiceResponse.java
@@ -2,6 +2,7 @@ package com.pororoz.istock.domain.user.dto.service;
 
 import com.pororoz.istock.domain.user.dto.response.UserResponse;
 import com.pororoz.istock.domain.user.entity.User;
+import java.time.LocalDateTime;
 import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +14,13 @@ public class UserServiceResponse {
   private Long id;
   private String username;
   private String roleName;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 
   public static UserServiceResponse of(User user) {
     return UserServiceResponse.builder().id(user.getId()).username(user.getUsername())
-        .roleName(user.getRole().getName()).build();
+        .roleName(user.getRole().getName()).createdAt(user.getCreatedAt())
+        .updatedAt(user.getUpdatedAt()).build();
   }
 
   public UserResponse toResponse() {
@@ -32,12 +36,13 @@ public class UserServiceResponse {
       return false;
     }
     UserServiceResponse that = (UserServiceResponse) o;
-    return Objects.equals(id, that.id) && Objects.equals(username, that.username) && Objects.equals(
-        roleName, that.roleName);
+    return Objects.equals(id, that.id) && Objects.equals(username, that.username)
+        && Objects.equals(roleName, that.roleName) && Objects.equals(createdAt,
+        that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, username, roleName);
+    return Objects.hash(id, username, roleName, createdAt, updatedAt);
   }
 }
