d46234affb9ab059b89c477f6d948858b1421641
dohyeon-han <limited.hyeon@gmail.com>
2023-01-25 01:17:41 +0900

    feat: Global Exception 추가 및 FindUserRequest valid 처리

--- src/main/java/com/pororoz/istock/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pororoz/istock/common/exception/GlobalExceptionHandler.java
@@ -5,9 +5,12 @@ import com.pororoz.istock.common.utils.message.ExceptionMessage;
 import com.pororoz.istock.common.utils.message.ExceptionStatus;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,94 +18,103 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // Validation
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException e) {
-        List<ErrorBinder> errors = new ArrayList<>();
-        for(FieldError error: e.getBindingResult().getFieldErrors()) {
-            errors.add(new ErrorBinder(error.getField(), error.getDefaultMessage()));
-        }
+  // @RequestBody valid
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleRequestBodyValidationErrors(
+      MethodArgumentNotValidException e) {
+    return getErrorResponseResponseEntity(e);
+  }
 
-        ErrorResponse response = ErrorResponse.builder()
-                .status(ExceptionStatus.BAD_REQUEST)
-                .message(errors.get(0).getMessage())
-                .errors(errors)
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  // @ModelAttribute valid
+  @ExceptionHandler(BindException.class)
+  public ResponseEntity<ErrorResponse> handleModelAttributeValidationErrors(
+      BindException e) {
+    return getErrorResponseResponseEntity(e);
+  }
+
+  private ResponseEntity<ErrorResponse> getErrorResponseResponseEntity(BindException e) {
+    List<ErrorBinder> errors = new ArrayList<>();
+    for (FieldError error : e.getBindingResult().getFieldErrors()) {
+      errors.add(new ErrorBinder(error.getField(), error.getDefaultMessage()));
     }
 
-    //
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ErrorResponse> handleValidationBadPath(ConstraintViolationException e) {
-        List<ErrorBinder> errors = new ArrayList<>();
-        for (ConstraintViolation error: e.getConstraintViolations()) {
-            errors.add(new ErrorBinder(error.getPropertyPath().toString(), error.getMessage()));
-        }
-        ErrorResponse response = ErrorResponse.builder()
-                .status(ExceptionStatus.BAD_REQUEST)
-                .message(errors.get(0).getMessage())
-                .errors(errors)
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
+    ErrorResponse response = ErrorResponse.builder()
+        .status(ExceptionStatus.BAD_REQUEST)
+        .message(errors.get(0).getMessage())
+        .errors(errors)
+        .build();
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
 
-    // Type Mismatch Validation
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponse> handleValidationTypeMismatchErrors(MethodArgumentTypeMismatchException e) {
-        ErrorResponse response = ErrorResponse.builder()
-                .status(ExceptionStatus.BAD_REQUEST)
-                .message(ExceptionMessage.TYPE_MISMATCH)
-                .errors(new ArrayList<>())
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  //
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<ErrorResponse> handleValidationBadPath(ConstraintViolationException e) {
+    List<ErrorBinder> errors = new ArrayList<>();
+    for (ConstraintViolation error : e.getConstraintViolations()) {
+      errors.add(new ErrorBinder(error.getPropertyPath().toString(), error.getMessage()));
     }
+    ErrorResponse response = ErrorResponse.builder()
+        .status(ExceptionStatus.BAD_REQUEST)
+        .message(errors.get(0).getMessage())
+        .errors(errors)
+        .build();
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
 
-    // Custom exception
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomErrors(final CustomException e){
-        ErrorResponse response = ErrorResponse.builder()
-                .status(e.getStatus())
-                .message(e.getMessage())
-                .build();
-        return new ResponseEntity<>(response, e.getStatusCode());
-    }
+  // Type Mismatch Validation
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<ErrorResponse> handleValidationTypeMismatchErrors() {
+    ErrorResponse response = ErrorResponse.builder()
+        .status(ExceptionStatus.BAD_REQUEST)
+        .message(ExceptionMessage.TYPE_MISMATCH)
+        .errors(new ArrayList<>())
+        .build();
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
 
-    // 404
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundApi() {
-        ErrorResponse response = ErrorResponse.builder()
-                .status(ExceptionStatus.PAGE_NOT_FOUND)
-                .message(ExceptionMessage.PAGE_NOT_FOUND)
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
+  // Custom exception
+  @ExceptionHandler(CustomException.class)
+  public ResponseEntity<ErrorResponse> handleCustomErrors(final CustomException e) {
+    ErrorResponse response = ErrorResponse.builder()
+        .status(e.getStatus())
+        .message(e.getMessage())
+        .build();
+    return new ResponseEntity<>(response, e.getStatusCode());
+  }
 
-    // Custom Exception 에서 처리되지 않은 400
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeError(final RuntimeException e){
-        log.error("Uncontrolled Exception", e);
-        ErrorResponse response = ErrorResponse.builder()
-                .status(ExceptionStatus.RUNTIME_ERROR)
-                .message(e.getMessage())
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
+  // 404
+  @ExceptionHandler(NoHandlerFoundException.class)
+  public ResponseEntity<ErrorResponse> handleNotFoundApi() {
+    ErrorResponse response = ErrorResponse.builder()
+        .status(ExceptionStatus.PAGE_NOT_FOUND)
+        .message(ExceptionMessage.PAGE_NOT_FOUND)
+        .build();
+    return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+  }
 
-    // 500
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> unhandledException(final Exception e){
-        log.error("Uncontrolled Exception", e);
-        ErrorResponse response = ErrorResponse.builder()
-                .status(ExceptionStatus.INTERNAL_SERVER_ERROR)
-                .message(e.getMessage())
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  // Custom Exception 에서 처리되지 않은 400
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<ErrorResponse> handleRuntimeError(final RuntimeException e) {
+    log.error("Uncontrolled Exception", e);
+    ErrorResponse response = ErrorResponse.builder()
+        .status(ExceptionStatus.RUNTIME_ERROR)
+        .message(e.getMessage())
+        .build();
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+  }
+
+  // 500
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> unhandledException(final Exception e) {
+    log.error("Uncontrolled Exception", e);
+    ErrorResponse response = ErrorResponse.builder()
+        .status(ExceptionStatus.INTERNAL_SERVER_ERROR)
+        .message(e.getMessage())
+        .build();
+    return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
--- src/main/java/com/pororoz/istock/domain/user/dto/request/FindUserRequest.java
+++ src/main/java/com/pororoz/istock/domain/user/dto/request/FindUserRequest.java
@@ -1,6 +1,9 @@
 package com.pororoz.istock.domain.user.dto.request;
 
+import com.pororoz.istock.common.utils.message.ExceptionMessage;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.PageRequest;
@@ -11,8 +14,10 @@ import org.springframework.data.domain.PageRequest;
 public class FindUserRequest {
 
   @Schema(description = "사용자 이름", example = "0")
+  @PositiveOrZero(message = ExceptionMessage.INVALID_PAGE_REQUEST)
   private int page;
   @Schema(description = "사용자 이름", example = "20")
+  @Positive(message = ExceptionMessage.INVALID_PAGE_REQUEST)
   private int size;
 
   public PageRequest toPageRequest() {
