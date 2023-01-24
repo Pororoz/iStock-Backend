4a5607711ebf37e01a028851f2effe9fb40585cb
dohyeon-han <limited.hyeon@gmail.com>
2023-01-24 15:00:24 +0900

    feat: User entity 저장 및 수정 시간 추가

--- src/main/java/com/pororoz/istock/domain/user/entity/User.java
+++ src/main/java/com/pororoz/istock/domain/user/entity/User.java
@@ -1,37 +1,57 @@
 package com.pororoz.istock.domain.user.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.io.Serializable;
-
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
+@Getter
+@Setter
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @NotNull
-    @Size(min = 4, max = 20)
-    @Column(length = 50, unique = true, nullable = false)
-    private String username;
+  @NotNull
+  @Size(min = 4, max = 20)
+  @Column(length = 50, unique = true, nullable = false)
+  private String username;
 
-    @NotNull
-    @Size(min = 4, max = 100)
-    @Column(nullable = false)
-    private String password;
+  @NotNull
+  @Size(min = 4, max = 100)
+  @Column(nullable = false)
+  private String password;
 
-    @NotNull
-    @ManyToOne(fetch = FetchType.EAGER)
-    private Role role;
+  @NotNull
+  @ManyToOne(fetch = FetchType.EAGER)
+  private Role role;
+
+  @CreatedDate
+  @Column(updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
 }
---
+++ src/test/java/com/pororoz/istock/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,81 @@
+package com.pororoz.istock.domain.user.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.pororoz.istock.domain.user.entity.Role;
+import com.pororoz.istock.domain.user.entity.User;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+  @Autowired
+  TestEntityManager em;
+  @Autowired
+  RoleRepository roleRepository;
+  @Autowired
+  UserRepository userRepository;
+
+
+  @Nested
+  @DisplayName("Commit time 테스트")
+  class TestEntitySaveTime {
+
+    User user;
+
+    @BeforeEach
+    void setUp() {
+      Role role = roleRepository.findByName("user").orElseThrow();
+      user = User.builder().username("user1").password("12345678").role(role).build();
+    }
+
+    @Test
+    @DisplayName("createdAt에 entity가 저장된 시간이 저장된다.")
+    void createdAt() {
+      //given
+      //when
+      User save = userRepository.save(user);
+
+      //then
+      assertEquals(save.getCreatedAt(), save.getUpdatedAt());
+      assertEquals(save.getUpdatedAt().getYear(), LocalDateTime.now().getYear());
+      assertEquals(save.getUpdatedAt().getMonth(), LocalDateTime.now().getMonth());
+      assertEquals(save.getUpdatedAt().getDayOfMonth(), LocalDateTime.now().getDayOfMonth());
+      assertEquals(save.getUpdatedAt().getHour(), LocalDateTime.now().getHour());
+      assertEquals(save.getUpdatedAt().getMinute(), LocalDateTime.now().getMinute());
+    }
+
+    @Test
+    @DisplayName("updateAt에 entity가 변경된 시간이 저장된다.")
+    void updatedAt() {
+      //given
+      User save = userRepository.save(user);
+
+      //when
+      save.setUsername("aaaaaaa");
+      em.flush();
+      em.clear();
+
+      //then
+      assertNotEquals(save.getCreatedAt(), save.getUpdatedAt());
+      assertTrue(save.getCreatedAt().isBefore(save.getUpdatedAt()));
+      assertEquals(save.getUpdatedAt().getYear(), LocalDateTime.now().getYear());
+      assertEquals(save.getUpdatedAt().getMonth(), LocalDateTime.now().getMonth());
+      assertEquals(save.getUpdatedAt().getDayOfMonth(), LocalDateTime.now().getDayOfMonth());
+      assertEquals(save.getUpdatedAt().getHour(), LocalDateTime.now().getHour());
+      assertEquals(save.getUpdatedAt().getMinute(), LocalDateTime.now().getMinute());
+    }
+  }
+
+}
