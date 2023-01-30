fc6af67342fe48ea7d00b8bab2373c0a9381212c
pythonstrup <pythonstrup@gmail.com>
2023-01-30 18:20:18 +0900

    feat: Category Entity에 createAt과 updatedAt 추가

--- src/main/java/com/pororoz/istock/domain/category/entity/Category.java
+++ src/main/java/com/pororoz/istock/domain/category/entity/Category.java
@@ -3,10 +3,14 @@ package com.pororoz.istock.domain.category.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.LocalDateTimeConverter;
 
 @Builder
 @AllArgsConstructor
@@ -24,5 +28,11 @@ public class Category {
   @Column(unique = true, nullable = false)
   private String name;
 
+  @CreatedDate
+  @Convert(converter = LocalDateTimeConverter.class)
+  private LocalDateTime createdAt;
 
+  @LastModifiedDate
+  @Convert(converter = LocalDateTimeConverter.class)
+  private LocalDateTime updatedAt;
 }
