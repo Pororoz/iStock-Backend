46736b81808ebd2806003a35081698ae3d7052aa
pythonstrup <pythonstrup@gmail.com>
2023-01-30 21:59:00 +0900

    feat: FindCategoryResponse 작성

---
+++ src/main/java/com/pororoz/istock/domain/category/dto/response/FindCategoryResponse.java
@@ -0,0 +1,36 @@
+package com.pororoz.istock.domain.category.dto.response;
+
+import java.util.Objects;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class FindCategoryResponse extends CategoryResponse{
+
+  private String createdAt;
+
+  private String updatedAt;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    if (!super.equals(obj)) {
+      return false;
+    }
+
+    FindCategoryResponse that = (FindCategoryResponse) obj;
+    return super.getId().equals(that.getId()) && super.getName().equals(that.getName()) &&
+        Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.getId(), super.getName(), createdAt, updatedAt);
+  }
+}
