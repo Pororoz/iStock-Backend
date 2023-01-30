f2e1983523d7012769df40d1a41316300d2e2a00
pythonstrup <pythonstrup@gmail.com>
2023-01-30 18:33:36 +0900

    feat: CategoryRepository findAllByName()
    - 그냥 테스트 돌아갈 수 있게만 만들어놓기

--- src/main/java/com/pororoz/istock/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/pororoz/istock/domain/category/repository/CategoryRepository.java
@@ -1,8 +1,10 @@
 package com.pororoz.istock.domain.category.repository;
 
 import com.pororoz.istock.domain.category.entity.Category;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+  PageImpl<Category> findAllByName(PageRequest pageRequest);
 }
