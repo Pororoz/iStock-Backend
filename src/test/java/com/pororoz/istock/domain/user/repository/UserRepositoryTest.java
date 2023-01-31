60beebd5c023bdc00e57df0e1866a65374bf0407
dohyeon-han <limited.hyeon@gmail.com>
2023-01-31 21:03:34 +0900

    chore: JpaAuditingConfiguration 삭제

--- src/test/java/com/pororoz/istock/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/com/pororoz/istock/domain/user/repository/UserRepositoryTest.java
@@ -15,10 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @DataJpaTest
-@EnableJpaAuditing
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
