51ca0629a340ee51cbf34f665727cf10488f2a4d
dohyeon-han <limited.hyeon@gmail.com>
2023-02-01 20:10:53 +0900

    feat: Part entity 및 repository

--- src/main/java/com/pororoz/istock/domain/part/entity/Part.java
+++ src/main/java/com/pororoz/istock/domain/part/entity/Part.java
@@ -1,14 +1,11 @@
-package com.pororoz.istock.domain.product.entity;
+package com.pororoz.istock.domain.part.entity;
 
 import com.pororoz.istock.common.entity.TimeEntity;
-import com.pororoz.istock.domain.category.entity.Category;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
@@ -22,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product extends TimeEntity {
+public class Part extends TimeEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,11 +30,14 @@ public class Product extends TimeEntity {
   private String name;
 
   @NotNull
-  @Size(max = 200)
-  private String productNumber;
+  @Size(max = 255)
+  private String spec;
 
-  @Size(max = 20)
-  private String codeNumber;
+  @NotNull
+  @PositiveOrZero
+  @Builder.Default
+  @Column(columnDefinition = "INT(11) UNSIGNED default 0")
+  private long price = 0;
 
   @NotNull
   @PositiveOrZero
@@ -45,9 +45,6 @@ public class Product extends TimeEntity {
   @Column(columnDefinition = "INT(11) UNSIGNED default 0")
   private long stock = 0;
 
-  @Size(max = 50)
+  @Size(min = 1, max = 50)
   private String companyName;
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  private Category category;
 }
