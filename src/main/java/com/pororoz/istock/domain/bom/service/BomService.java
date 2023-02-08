51276e6b9e7199c5a89b91b87349487bd8f0d335
pythonstrup <pythonstrup@gmail.com>
2023-02-08 11:57:54 +0900

    feat: BomService 작성 및 테스트 성공

---
+++ src/main/java/com/pororoz/istock/domain/bom/service/BomService.java
@@ -0,0 +1,32 @@
+package com.pororoz.istock.domain.bom.service;
+
+import com.pororoz.istock.domain.bom.dto.service.SaveBomServiceRequest;
+import com.pororoz.istock.domain.bom.dto.service.SaveBomServiceResponse;
+import com.pororoz.istock.domain.bom.entity.Bom;
+import com.pororoz.istock.domain.bom.repository.BomRepository;
+import com.pororoz.istock.domain.part.entity.Part;
+import com.pororoz.istock.domain.part.repository.PartRepository;
+import com.pororoz.istock.domain.product.entity.Product;
+import com.pororoz.istock.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class BomService {
+
+  private final PartRepository partRepository;
+  private final ProductRepository productRepository;
+  private final BomRepository bomRepository;
+
+  public SaveBomServiceResponse saveBom(SaveBomServiceRequest request) {
+    Part part = partRepository.findById(request.getPartId())
+        .orElseThrow();
+    Product product = productRepository.findById(request.getProductId())
+        .orElseThrow();
+    Bom result = bomRepository.save(request.toBom(part, product));
+    return SaveBomServiceResponse.of(result);
+  }
+}
