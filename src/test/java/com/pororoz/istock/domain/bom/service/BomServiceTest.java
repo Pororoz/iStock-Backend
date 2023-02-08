57f29e35899c32333a2e1d64c75e8dcf9f381523
pythonstrup <pythonstrup@gmail.com>
2023-02-08 11:35:40 +0900

    feat: BomServiceTest 작성

---
+++ src/test/java/com/pororoz/istock/domain/bom/service/BomServiceTest.java
@@ -0,0 +1,114 @@
+package com.pororoz.istock.domain.bom.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.pororoz.istock.domain.bom.entity.Bom;
+import com.pororoz.istock.domain.bom.repository.BomRepository;
+import com.pororoz.istock.domain.part.entity.Part;
+import com.pororoz.istock.domain.part.repository.PartRepository;
+import com.pororoz.istock.domain.product.entity.Product;
+import com.pororoz.istock.domain.product.repository.ProductRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class BomServiceTest {
+
+  @InjectMocks
+  BomService bomService;
+
+  @Mock
+  BomRepository bomRepository;
+
+  @Mock
+  PartRepository partRepository;
+
+  @Mock
+  ProductRepository productRepository;
+
+  @Nested
+  @DisplayName("제품 BOM 행 추가 로직 테스트")
+  class SaveBom {
+
+    Long bomId;
+    String locationNumber;
+    String codeNumber;
+    Long quantity;
+    String memo;
+    Long partId;
+    Long productId;
+
+    @BeforeEach
+    void setup() {
+      bomId = 1L;
+      locationNumber = "L5.L4";
+      codeNumber = "";
+      quantity = 3L;
+      memo = "";
+      partId = 1L;
+      productId = 2L;
+    }
+
+    @Nested
+    @DisplayName("성공 케이스")
+    class SuccessCase {
+      @Test
+      @DisplayName("입력값으로 적절한 값이 들어오면 BOM이 정상적으로 추가된다.")
+      void saveBom() {
+        // given
+        SaveBomServiceRequest request = SaveBomServiceRequest.builder()
+            .locationNumber(locationNumber)
+            .codeNumber(codeNumber)
+            .quantity(quantity)
+            .memo(memo)
+            .partId(partId)
+            .productId(productId)
+            .build();
+        SaveBomServiceResponse response = SaveBomServiceResponse.builder()
+            .bomId(bomId)
+            .locationNumber(locationNumber)
+            .codeNumber(codeNumber)
+            .quantity(quantity)
+            .memo(memo)
+            .partId(partId)
+            .productId(productId)
+            .build();
+
+        Part part = Part.builder().id(partId).build();
+        Product product = Product.builder().id(productId).build();
+        Bom bom = Bom.builder()
+            .id(bomId)
+            .locationNumber(locationNumber)
+            .codeNumber(codeNumber)
+            .quantity(quantity)
+            .memo(memo)
+            .part(part)
+            .product(product)
+            .build();
+
+        // when
+        when(partRepository.findById(any())).thenReturn(Optional.of(part));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(bomRepository.save(any())).thenReturn(bom);
+        SaveBomServiceResponse result = bomService.saveBom(request);
+
+        // then
+        assertThat(result).usingRecursiveComparison().isEqualTo(response);
+      }
+    }
+
+    @Nested
+    @DisplayName("실패 케이스")
+    class FailCase {
+
+    }
+  }
+}
