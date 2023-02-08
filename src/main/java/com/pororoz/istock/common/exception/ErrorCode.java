66ff4bed01bd01b04355a43fe8487131974daf00
Park HyeonJun <darkeroe@hanyang.ac.kr>
2023-02-08 20:04:26 +0900

    chore: indentation error 수정

--- src/main/java/com/pororoz/istock/common/exception/ErrorCode.java
+++ src/main/java/com/pororoz/istock/common/exception/ErrorCode.java
@@ -11,15 +11,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, ExceptionStatus.PAGE_NOT_FOUND, ExceptionMessage.PAGE_NOT_FOUND),
-
     ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, ExceptionStatus.ROLE_NOT_FOUND, ExceptionMessage.ROLE_NOT_FOUND),
-
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, ExceptionStatus.USER_NOT_FOUND, ExceptionMessage.USER_NOT_FOUND),
-
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, ExceptionStatus.CATEGORY_NOT_FOUND, ExceptionMessage.CATEGORY_NOT_FOUND),
-
     PART_NAME_DUPLICATED(HttpStatus.BAD_REQUEST, ExceptionStatus.PART_NAME_DUPLICATED, ExceptionMessage.PART_NAME_DUPLICATED);
-
 
     private final HttpStatus statusCode;
 
