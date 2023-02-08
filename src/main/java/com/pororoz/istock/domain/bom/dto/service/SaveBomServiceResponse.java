e9952df7755afe5d6525b25813176066e10edf57
pythonstrup <pythonstrup@gmail.com>
2023-02-08 11:51:19 +0900

    feat: SaveBomServiceResponse 작성

---
+++ src/main/java/com/pororoz/istock/domain/bom/dto/service/SaveBomServiceResponse.java
@@ -0,0 +1,30 @@
+package com.pororoz.istock.domain.bom.dto.service;
+
+import com.pororoz.istock.domain.bom.entity.Bom;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SaveBomServiceResponse {
+
+  private Long bomId;
+  private String locationNumber;
+  private String codeNumber;
+  private Long quantity;
+  private String memo;
+  private Long partId;
+  private Long productId;
+
+  public static SaveBomServiceResponse of(Bom bom) {
+    return SaveBomServiceResponse.builder()
+        .bomId(bom.getId())
+        .locationNumber(bom.getLocationNumber())
+        .codeNumber(bom.getCodeNumber())
+        .quantity(bom.getQuantity())
+        .memo(bom.getMemo())
+        .partId(bom.getPart().getId())
+        .productId(bom.getProduct().getId())
+        .build();
+  }
+}
