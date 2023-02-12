246c5145fe7d3753fcd9980110195df6ea59a619
pythonstrup <pythonstrup@gmail.com>
2023-02-12 22:55:40 +0900

    feat: BOM DELETE Swagger 작성

--- src/main/java/com/pororoz/istock/domain/bom/controller/BomController.java
+++ src/main/java/com/pororoz/istock/domain/bom/controller/BomController.java
@@ -10,7 +10,9 @@ import com.pororoz.istock.domain.bom.dto.request.SaveBomRequest;
 import com.pororoz.istock.domain.bom.dto.response.BomResponse;
 import com.pororoz.istock.domain.bom.dto.service.BomServiceResponse;
 import com.pororoz.istock.domain.bom.service.BomService;
+import com.pororoz.istock.domain.bom.swagger.exception.BomNotFoundExceptionSwagger;
 import com.pororoz.istock.domain.bom.swagger.exception.NotExistedPartExceptionSwagger;
+import com.pororoz.istock.domain.bom.swagger.response.DeleteBomResponseSwagger;
 import com.pororoz.istock.domain.bom.swagger.response.SaveBomResponseSwagger;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -57,6 +59,18 @@ public class BomController {
         new ResultDTO<>(ResponseStatus.OK, ResponseMessage.SAVE_BOM, response));
   }
 
+  @Operation(summary = "save bom", description = "BOM 행 추가 API")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = ResponseMessage.DELETE_BOM, content = {
+          @Content(schema = @Schema(implementation = DeleteBomResponseSwagger.class))}
+      ),
+      @ApiResponse(responseCode = "400", description = ExceptionMessage.BOM_NOT_FOUND, content = {
+          @Content(schema = @Schema(implementation = BomNotFoundExceptionSwagger.class))}
+      ),
+      @ApiResponse(responseCode = "403", description = ExceptionMessage.FORBIDDEN, content = {
+          @Content(schema = @Schema(implementation = AccessForbiddenSwagger.class))}
+      ),
+  })
   @DeleteMapping
   public ResponseEntity<ResultDTO<BomResponse>> deleteBom(
       @Valid @ModelAttribute("request") DeleteBomRequest request) {
--- src/main/java/com/pororoz/istock/domain/bom/dto/request/DeleteBomRequest.java
+++ src/main/java/com/pororoz/istock/domain/bom/dto/request/DeleteBomRequest.java
@@ -1,6 +1,7 @@
 package com.pororoz.istock.domain.bom.dto.request;
 
 import com.pororoz.istock.domain.bom.dto.service.DeleteBomServiceRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
@@ -10,6 +11,7 @@ import lombok.Getter;
 @Builder
 public class DeleteBomRequest {
 
+  @Schema(description = "BOM 아이디", example = "1")
   @NotNull
   @PositiveOrZero
   private Long bomId;
