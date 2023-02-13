8c23a226b46fb62c7aa97008c18c36622f9f1742
pythonstrup <pythonstrup@gmail.com>
2023-02-13 15:20:05 +0900

    feat: UpdateBomServiceRequest 작성

---
+++ src/main/java/com/pororoz/istock/domain/bom/dto/service/UpdateBomServiceRequest.java
@@ -0,0 +1,16 @@
+package com.pororoz.istock.domain.bom.dto.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class UpdateBomServiceRequest {
+  private Long bomId;
+  private String locationNumber;
+  private String codeNumber;
+  private Long quantity;
+  private String memo;
+  private Long partId;
+  private Long productId;
+}
