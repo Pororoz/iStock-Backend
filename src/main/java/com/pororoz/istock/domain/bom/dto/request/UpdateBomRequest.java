851184c5c1be8b51446050a933ff86ba0af3a80d
pythonstrup <pythonstrup@gmail.com>
2023-02-13 15:46:45 +0900

    feat: UpdateBomRequest 작성

---
+++ src/main/java/com/pororoz/istock/domain/bom/dto/request/UpdateBomRequest.java
@@ -0,0 +1,57 @@
+package com.pororoz.istock.domain.bom.dto.request;
+
+import com.pororoz.istock.domain.bom.dto.service.UpdateBomServiceRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateBomRequest {
+  @Schema(description = "BOM 아이디", example = "1")
+  @NotNull
+  @PositiveOrZero
+  private Long bomId;
+
+  @Schema(description = "location 번호", example = "L5.L4")
+  @Size(max = 20)
+  private String locationNumber = "0";
+
+  @Schema(description = "코드 번호", example = "0A")
+  @Size(max = 20)
+  private String codeNumber;
+
+  @Schema(description = "제품 수량", example = "3")
+  @PositiveOrZero
+  private long quantity = 0;
+
+  @Schema(description = "비고", example = "비고")
+  private String memo;
+
+  @Schema(description = "part 아이다", example = "1")
+  @NotNull
+  private Long partId;
+
+  @Schema(description = "product 아이디", example = "2")
+  @NotNull
+  private Long productId;
+
+  public UpdateBomServiceRequest toService() {
+    return UpdateBomServiceRequest.builder()
+        .bomId(bomId)
+        .locationNumber(locationNumber)
+        .codeNumber(codeNumber)
+        .quantity(quantity)
+        .memo(memo)
+        .partId(partId)
+        .productId(productId)
+        .build();
+  }
+}
