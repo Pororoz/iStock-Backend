7f01c3ac1fdda26506f2f837c69bf294341eebd1
Park HyeonJun <darkeroe@hanyang.ac.kr>
2023-02-23 16:58:44 +0900

    feat: 제품 자재 일괄 구매 통합테스트 작성

---
+++ src/test/java/com/pororoz/istock/domain/purchase/PurchaseIntegrationTest.java
@@ -0,0 +1,155 @@
+package com.pororoz.istock.domain.purchase;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.pororoz.istock.IntegrationTest;
+import com.pororoz.istock.common.utils.message.ResponseMessage;
+import com.pororoz.istock.common.utils.message.ResponseStatus;
+import com.pororoz.istock.domain.bom.entity.Bom;
+import com.pororoz.istock.domain.bom.repository.BomRepository;
+import com.pororoz.istock.domain.category.entity.Category;
+import com.pororoz.istock.domain.category.repository.CategoryRepository;
+import com.pororoz.istock.domain.part.dto.request.SavePartRequest;
+import com.pororoz.istock.domain.part.entity.Part;
+import com.pororoz.istock.domain.part.repository.PartRepository;
+import com.pororoz.istock.domain.product.entity.Product;
+import com.pororoz.istock.domain.product.repository.ProductRepository;
+import com.pororoz.istock.domain.purchase.dto.request.PurchaseProductRequest;
+import com.pororoz.istock.domain.purchase.dto.response.PurchaseProductResponse;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.ResultActions;
+
+public class PurchaseIntegrationTest extends IntegrationTest {
+
+  @Autowired
+  ProductRepository productRepository;
+
+  @Autowired
+  BomRepository bomRepository;
+
+  @Autowired
+  PartRepository partRepository;
+
+  @Autowired
+  CategoryRepository categoryRepository;
+
+  @Nested
+  @DisplayName("POST /v1/purchase/product - 제품 자재 일괄 구매")
+  class PurchaseProduct {
+
+    private final String url = "http://localhost:8080/v1/purchase/product";
+
+    @BeforeEach
+    void setUp() {
+      databaseCleanup.execute();
+      Category category = categoryRepository.save(Category.builder().categoryName("카테고리").build());
+      //product 저장
+      List<Product> products = new ArrayList<>();
+      for (int i = 0; i < 10; i++) {
+        Product product = productRepository.save(Product.builder()
+            .productName("p" + i).productNumber("p" + i)
+            .stock((int) (Math.random() * 100) + 1).category(category)
+            .build());
+        products.add(product);
+      }
+      //part 저장
+      List<Part> parts = new ArrayList<>();
+      for (int i = 0; i < 10; i++) {
+        Part part = partRepository.save(Part.builder()
+            .partName("p" + i).spec("p" + i)
+            .build());
+        parts.add(part);
+      }
+      //일반 bom
+      for (int i = 0; i < 9; i++) {
+        bomRepository.save(Bom.builder()
+            .codeNumber("10").locationNumber("" + i + 100)
+            .part(parts.get((int) (Math.random() * 9) + 0))
+            .product(products.get((int) (Math.random() * 9) + 0))
+            .build());
+      }
+    }
+
+    @Nested
+    @DisplayName("성공 케이스")
+    class SuccessCase {
+
+      @Test
+      @WithMockUser(roles = "ADMIN")
+      @DisplayName("제품 자재 일괄 구매 요청에 성공한다.")
+      void purchaseProduct() throws Exception {
+        //given
+        PurchaseProductRequest request = PurchaseProductRequest.builder()
+            .productId(1L)
+            .amount(100L)
+            .build();
+
+        PurchaseProductResponse response = PurchaseProductResponse.builder()
+            .productId(1L)
+            .amount(100L)
+            .build();
+
+        //when
+        ResultActions actions = getResultActions(url, HttpMethod.POST, request);
+
+        //then
+        actions.andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value(ResponseStatus.OK))
+            .andExpect(jsonPath("$.message").value(ResponseMessage.PURCHASE_PRODUCT))
+            .andExpect(jsonPath("$.data", equalTo(asParsedJson(response))))
+            .andDo(print());
+      }
+    }
+
+    @Nested
+    @DisplayName("실패 케이스")
+    class FailCase {
+
+      @Test
+      @WithMockUser(roles = "ADMIN")
+      @DisplayName("존재하지 않는 Product를 넘겨주면 구매 요청에 실패한다.")
+      void productNotFound() throws Exception {
+        //given
+        PurchaseProductRequest request = PurchaseProductRequest.builder()
+            .productId(100L)
+            .amount(100L)
+            .build();
+
+        //when
+        ResultActions actions = getResultActions(url, HttpMethod.POST, request);
+
+        //then
+        actions.andExpect(status().isNotFound())
+            .andDo(print());
+      }
+
+      @Test
+      @DisplayName("인증되지 않은 사용자가 접근하면 FORBIDDEN을 반환한다.")
+      void forbidden() throws Exception {
+        //given
+        PurchaseProductRequest request = PurchaseProductRequest.builder()
+            .productId(1L)
+            .amount(100L)
+            .build();
+
+        //when
+        ResultActions actions = getResultActions(url, HttpMethod.POST, request);
+
+        //then
+        actions.andExpect(status().isForbidden())
+            .andDo(print());
+      }
+    }
+  }
+}
