050159beaaa5f7988a875ccde93c2b02fc5cd074
Park HyeonJun <darkeroe@hanyang.ac.kr>
2023-02-24 15:39:38 +0900

    feat: 제품 자재 개별 구매 Swagger 추가

--- src/main/java/com/pororoz/istock/domain/purchase/controller/PurchaseController.java
+++ src/main/java/com/pororoz/istock/domain/purchase/controller/PurchaseController.java
@@ -5,6 +5,7 @@ import com.pororoz.istock.common.swagger.exception.AccessForbiddenSwagger;
 import com.pororoz.istock.common.utils.message.ExceptionMessage;
 import com.pororoz.istock.common.utils.message.ResponseMessage;
 import com.pororoz.istock.common.utils.message.ResponseStatus;
+import com.pororoz.istock.domain.part.exception.PartNotFoundException;
 import com.pororoz.istock.domain.product.exception.ProductNotFoundException;
 import com.pororoz.istock.domain.purchase.dto.request.PurchasePartRequest;
 import com.pororoz.istock.domain.purchase.dto.request.PurchaseProductRequest;
@@ -58,6 +59,14 @@ public class PurchaseController {
   }
 
   @Operation(summary = "purchase part", description = "제품 자재 개별 구매 API")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = ResponseMessage.PURCHASE_PART, content = {
+          @Content(schema = @Schema(implementation = PurchaseProductResponseSwagger.class))}),
+      @ApiResponse(responseCode = "403", description = ExceptionMessage.FORBIDDEN, content = {
+          @Content(schema = @Schema(implementation = AccessForbiddenSwagger.class))}),
+      @ApiResponse(responseCode = "404", description = ExceptionMessage.PART_NOT_FOUND, content = {
+          @Content(schema = @Schema(implementation = PartNotFoundException.class))})
+  })
   @PostMapping("/part")
   public ResponseEntity<ResultDTO<PurchasePartResponse>> purchasePart(
       @Valid @RequestBody PurchasePartRequest purchasePartRequest) {
