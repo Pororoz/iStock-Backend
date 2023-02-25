41ff1dd7f225da5809c0d7f920d313a26aacdc79
pythonstrup <pythonstrup@gmail.com>
2023-02-25 14:31:10 +0900

    feat: LocaleDateTime 직렬화

--- src/main/java/com/pororoz/istock/domain/bom/dto/response/FindBomResponse.java
+++ src/main/java/com/pororoz/istock/domain/bom/dto/response/FindBomResponse.java
@@ -1,5 +1,9 @@
 package com.pororoz.istock.domain.bom.dto.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.pororoz.istock.domain.part.entity.Part;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -17,7 +21,13 @@ public class FindBomResponse {
   private String codeNumber;
   private Long quantity;
   private String memo;
+
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
   private LocalDateTime createdAt;
+
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
   private LocalDateTime updatedAt;
   private Part part;
 }
