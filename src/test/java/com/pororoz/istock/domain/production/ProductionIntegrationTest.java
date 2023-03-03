7d160eee7076bf8d789aea1f2a2f22d0d7470644
dohyeon-han <limited.hyeon@gmail.com>
2023-03-03 22:01:58 +0900

    feat: 통합 테스트 추가

--- src/test/java/com/pororoz/istock/domain/production/ProductionIntegrationTest.java
+++ src/test/java/com/pororoz/istock/domain/production/ProductionIntegrationTest.java
@@ -25,6 +25,7 @@ import com.pororoz.istock.domain.product.repository.ProductIoRepository;
 import com.pororoz.istock.domain.product.repository.ProductRepository;
 import com.pororoz.istock.domain.production.dto.request.SaveProductionRequest;
 import com.pororoz.istock.domain.production.dto.response.SaveProductionResponse;
+import com.pororoz.istock.domain.production.dto.response.UpdateProductionResponse;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -54,10 +55,6 @@ public class ProductionIntegrationTest extends IntegrationTest {
 
   @Autowired
   ProductIoRepository productIoRepository;
-
-  String getUri(Long productId) {
-    return "/v1/production/products/" + productId + "/waiting";
-  }
 
   Category category;
   Product product1, subAssy1, subAssy2;
@@ -108,10 +105,30 @@ public class ProductionIntegrationTest extends IntegrationTest {
         .build();
   }
 
+  ProductIo saveProductIo(long quantity, ProductStatus status, Product product) {
+    return productIoRepository.save(ProductIo.builder()
+        .quantity(quantity).status(status)
+        .product(product).build());
+  }
+
+  ProductIo saveSubAssyIo(long quantity, ProductStatus status, Product subAssy, ProductIo superIo) {
+    return productIoRepository.save(ProductIo.builder()
+        .quantity(quantity).status(status)
+        .product(subAssy).superIo(superIo)
+        .build());
+  }
+
   PartIo createPartIo(long quantity, PartStatus status) {
     return PartIo.builder()
         .quantity(quantity).status(status)
         .build();
+  }
+
+  PartIo savePartIo(long quantity, PartStatus status, Part part, ProductIo productIo) {
+    return partIoRepository.save(PartIo.builder()
+        .quantity(quantity).status(status)
+        .part(part).productIo(productIo)
+        .build());
   }
 
   @Nested
@@ -120,6 +137,10 @@ public class ProductionIntegrationTest extends IntegrationTest {
   class SaveWaitProduction {
 
     long quantity = 10L;
+
+    String getUri(Long productId) {
+      return "/v1/production/products/" + productId + "/waiting";
+    }
 
     @Test
     @DisplayName("제품 생산 대기 요청을 하면 부품과 subassy의 개수를 줄이고 결과를 반환한다.")
@@ -148,21 +169,72 @@ public class ProductionIntegrationTest extends IntegrationTest {
       assertThat(productRepository.findById(subAssy1.getId()).orElseThrow().getStock()).isZero();
       assertThat(productRepository.findById(subAssy2.getId()).orElseThrow().getStock()).isZero();
 
-      ProductIo productIo = createProductIo(quantity, ProductStatus.생산대기);
-      ProductIo subAssyIo1 = createProductIo(subAssyBom1.getQuantity() * quantity,
-          ProductStatus.사내출고대기);
-      ProductIo subAssyIo2 = createProductIo(subAssyBom2.getQuantity() * quantity,
-          ProductStatus.사내출고대기);
-      PartIo partIo1 = createPartIo(bom1.getQuantity() * quantity, PartStatus.생산대기);
-      PartIo partIo2 = createPartIo(bom2.getQuantity() * quantity, PartStatus.생산대기);
-
-      assertThat(partIoRepository.findAll()).usingRecursiveComparison()
-          .ignoringFields("createdAt", "updatedAt", "id", "part", "productIo")
-          .isEqualTo(List.of(partIo1, partIo2));
-      assertThat(productIoRepository.findAll()).usingRecursiveComparison()
-          .ignoringFields("createdAt", "updatedAt", "id", "product", "superIo", "partIoList",
-              "subAssyIoList")
-          .isEqualTo(List.of(productIo, subAssyIo1, subAssyIo2));
+      verifyPartIo(quantity, bom1.getQuantity(), bom2.getQuantity(), PartStatus.생산대기);
+      verifyProductIo(quantity, ProductStatus.생산대기, subAssyBom1.getQuantity(),
+          subAssyBom2.getQuantity(), ProductStatus.사내출고대기);
     }
+  }
+
+  @Nested
+  @WithMockUser
+  @DisplayName("POST - v1/production/product-io/{productIoId}/confirm 제품 생산 확정")
+  class ConfirmProduction {
+
+    String getUri(Long productIoId) {
+      return "/v1/production/product-io/" + productIoId + "/confirm";
+    }
+
+    @Test
+    @DisplayName("생산 확정 요청을 하고 productIo와 partIo의 상태를 변경한다.")
+    void confirmPartIo() throws Exception {
+      //given
+      long quantity = 5;
+      ProductIo productIo = saveProductIo(quantity, ProductStatus.생산대기, product1);
+      savePartIo(quantity, PartStatus.생산대기, part1, productIo);
+      savePartIo(quantity * 2, PartStatus.생산대기, part2, productIo);
+      saveSubAssyIo(quantity, ProductStatus.사내출고대기, subAssy1, productIo);
+      saveSubAssyIo(quantity * 2, ProductStatus.사내출고대기, subAssy2, productIo);
+
+      String uri = getUri(productIo.getId());
+
+      //when
+      ResultActions actions = getResultActions(uri, HttpMethod.POST);
+
+      //then
+      UpdateProductionResponse response = UpdateProductionResponse.builder()
+          .productIoId(productIo.getId())
+          .productId(product1.getId())
+          .quantity(quantity).build();
+
+      actions.andExpect(status().isOk())
+          .andExpect(jsonPath("$.status").value(ResponseStatus.OK))
+          .andExpect(jsonPath("$.message").value(ResponseMessage.CONFIRM_PRODUCTION))
+          .andExpect(jsonPath("$.data", equalTo(asParsedJson(response))))
+          .andDo(print());
+      assertThat(productRepository.findById(product1.getId()).orElseThrow().getStock())
+          .isEqualTo(product1.getStock() + quantity);
+      verifyPartIo(quantity, 1, 2, PartStatus.생산완료);
+      verifyProductIo(quantity, ProductStatus.생산완료, 1, 2, ProductStatus.사내출고완료);
+    }
+  }
+
+  void verifyProductIo(long quantity1, ProductStatus productStatus, long quantity2, long quantity3,
+      ProductStatus subAssyStatus) {
+    ProductIo productIo = createProductIo(quantity1, productStatus);
+    ProductIo subAssyIo1 = createProductIo(quantity2 * quantity1, subAssyStatus);
+    ProductIo subAssyIo2 = createProductIo(quantity3 * quantity1, subAssyStatus);
+    assertThat(productIoRepository.findAll()).usingRecursiveComparison()
+        .ignoringFields("createdAt", "updatedAt", "id", "product", "superIo", "partIoList",
+            "subAssyIoList")
+        .isEqualTo(List.of(productIo, subAssyIo1, subAssyIo2));
+  }
+
+  void verifyPartIo(long quantity1, long quantity2, long quantity3, PartStatus status) {
+    PartIo partIo1 = createPartIo(quantity2 * quantity1, status);
+    PartIo partIo2 = createPartIo(quantity3 * quantity1, status);
+
+    assertThat(partIoRepository.findAll()).usingRecursiveComparison()
+        .ignoringFields("createdAt", "updatedAt", "id", "part", "productIo")
+        .isEqualTo(List.of(partIo1, partIo2));
   }
 }
