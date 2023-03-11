33924258814326ed2093831755223aab89b18613
pythonstrup <pythonstrup@gmail.com>
2023-03-11 17:09:38 +0900

    feat: OutboundServiceResponse 작성

--- src/main/java/com/pororoz/istock/domain/outbound/dto/service/OutboundServiceResponse.java
+++ src/main/java/com/pororoz/istock/domain/outbound/dto/service/OutboundServiceResponse.java
@@ -1,5 +1,6 @@
 package com.pororoz.istock.domain.outbound.dto.service;
 
+import com.pororoz.istock.domain.product.entity.ProductIo;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,4 +10,11 @@ public class OutboundServiceResponse {
 
   private Long productId;
   private Long quantity;
+
+  public static OutboundServiceResponse of(ProductIo productIo) {
+    return OutboundServiceResponse.builder()
+        .productId(productIo.getProduct().getId())
+        .quantity(productIo.getQuantity())
+        .build();
+  }
 }
