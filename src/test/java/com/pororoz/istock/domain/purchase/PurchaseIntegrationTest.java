e675b55b06baf29c62df725ad4e129c615bcf378
Park HyeonJun <darkeroe@hanyang.ac.kr>
2023-03-15 21:17:29 +0900

    chore: 통합테스트 setup 수정

--- src/test/java/com/pororoz/istock/domain/purchase/PurchaseIntegrationTest.java
+++ src/test/java/com/pororoz/istock/domain/purchase/PurchaseIntegrationTest.java
@@ -69,65 +69,82 @@ public class PurchaseIntegrationTest extends IntegrationTest {
     //product 저장
     List<Product> products = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
-      Product product = productRepository.save(Product.builder()
-          .productName("p" + i).productNumber("p" + i)
-          .stock((int) (Math.random() * 100) + 1).category(category)
-          .codeNumber(String.valueOf(i))
-          .build());
+      Product product = Product.builder()
+          .productName("p" + i)
+          .productNumber("p" + i)
+          .stock((int) (Math.random() * 100) + 1)
+          .category(category)
+          .codeNumber(String.valueOf(2 + i))
+          .build();
+
       products.add(product);
     }
+    productRepository.saveAll(products);
+
     //part 저장
     List<Part> parts = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
-      Part part = partRepository.save(Part.builder()
-          .partName("p" + i).spec("p" + i)
-          .build());
+      Part part = Part.builder()
+          .partName("p" + i)
+          .spec("p" + i)
+          .build();
+
       parts.add(part);
     }
+    partRepository.saveAll(parts);
+
     //일반 bom
     for (int i = 0; i < 9; i++) {
-      bomRepository.save(Bom.builder()
+      Bom bom = Bom.builder()
           .quantity(1)
-          .codeNumber("10").locationNumber("" + i + 100)
+          .codeNumber("10")
+          .locationNumber("" + i + 100)
           .part(parts.get((int) (Math.random() * 9)))
           .product(products.get((int) (Math.random() * 9)))
-          .build());
+          .build();
+
+      bomRepository.save(bom);
     }
+
     //productIo 저장
-    ProductIo productIo = productIoRepository.save(ProductIo.builder()
+    ProductIo productIo = ProductIo.builder()
         .quantity(10)
         .status(ProductStatus.구매대기)
         .product(products.get(0))
         .superIo(null)
-        .build()
-    );
-    productIoRepository.save(ProductIo.builder()
+        .build();
+    productIoRepository.save(productIo);
+
+    ProductIo productIo2 = ProductIo.builder()
         .quantity(10)
         .status(ProductStatus.외주구매대기)
         .product(products.get(9))
         .superIo(productIo)
-        .build()
-    );
-    productIoRepository.save(ProductIo.builder()
+        .build();
+    productIoRepository.save(productIo2);
+
+    ProductIo productIo3 = ProductIo.builder()
         .quantity(10)
         .status(ProductStatus.외주구매확정)
         .product(products.get(9))
         .superIo(productIo)
-        .build()
-    );
+        .build();
+    productIoRepository.save(productIo3);
+
     //partIo 저장
-    partIoRepository.save(PartIo.builder()
+    PartIo partIo = PartIo.builder()
         .quantity(10)
         .status(PartStatus.구매대기)
         .part(parts.get(0))
-        .build()
-    );
-    partIoRepository.save(PartIo.builder()
+        .build();
+    partIoRepository.save(partIo);
+
+    PartIo partIo2 = PartIo.builder()
         .quantity(10)
         .status(PartStatus.구매확정)
         .part(parts.get(0))
-        .build()
-    );
+        .build();
+    partIoRepository.save(partIo2);
   }
 
   @Nested
