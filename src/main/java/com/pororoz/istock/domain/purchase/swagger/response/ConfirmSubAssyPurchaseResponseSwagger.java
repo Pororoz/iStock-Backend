07c9fd2fc48f1a4bfd7a4d00449ef88b63c2d53c
Park HyeonJun <darkeroe@hanyang.ac.kr>
2023-03-16 15:31:24 +0900

    chore: getter 추가

--- src/main/java/com/pororoz/istock/domain/purchase/swagger/response/ConfirmSubAssyPurchaseResponseSwagger.java
+++ src/main/java/com/pororoz/istock/domain/purchase/swagger/response/ConfirmSubAssyPurchaseResponseSwagger.java
@@ -4,7 +4,9 @@ import com.pororoz.istock.common.utils.message.ResponseMessage;
 import com.pororoz.istock.common.utils.message.ResponseStatus;
 import com.pororoz.istock.domain.product.dto.response.ProductResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
+@Getter
 public class ConfirmSubAssyPurchaseResponseSwagger {
 
   @Schema(description = "Result Code", example = ResponseStatus.OK)
