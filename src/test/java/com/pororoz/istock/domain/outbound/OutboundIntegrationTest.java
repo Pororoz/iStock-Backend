7b980cc0155d51a6b6f07a8cd58f6e21312e5501
pythonstrup <pythonstrup@gmail.com>
2023-03-18 18:31:48 +0900

    feat: OutboundIntegrationTest 작성
    - outbound cancel API

--- src/test/java/com/pororoz/istock/domain/outbound/OutboundIntegrationTest.java
+++ src/test/java/com/pororoz/istock/domain/outbound/OutboundIntegrationTest.java
@@ -281,4 +281,136 @@ public class OutboundIntegrationTest extends IntegrationTest {
       }
     }
   }
+
+  @Nested
+  @DisplayName("POST /api/v1/outbounds/product-io/{productIoId}/cancel - 제품 출고 취소")
+  class OutboundCancel {
+
+    String url(Long productIoId) {
+      return "http://localhost:8080/v1/outbounds/product-io/" + productIoId + "/cancel";
+    }
+
+    final Long productId = 1L;
+    final Long productIoId = 1L;
+    final Long productQuantity = 50L;
+    final Long productIoQuantity = 100L;
+    final String name = "product1";
+    final String number = "GS-IH-01";
+    final String companyName = "company name";
+    final String codeNumber = "code number";
+
+    @Nested
+    @DisplayName("성공 케이스")
+    class SuccessCase {
+      @BeforeEach
+      void setup() {
+        Category category = categoryRepository.save(
+            Category.builder().categoryName("카테고리").build());
+        Product product = productRepository.save(
+            Product.builder()
+                .productName(name)
+                .productNumber(number)
+                .codeNumber(codeNumber)
+                .companyName(companyName)
+                .stock(productQuantity)
+                .category(category)
+                .build());
+        productIoRepository.save(ProductIo.builder()
+            .status(ProductStatus.출고대기)
+            .quantity(productIoQuantity)
+            .product(product)
+            .build());
+      }
+
+      @Test
+      @WithMockUser(roles = "USER")
+      @DisplayName("productIoId, productId와 quantity가 포함된 정보를 제공하면 200 OK와 data를 전달한다.")
+      void outboundConfirm() throws Exception {
+        // given
+
+        // when
+        ResultActions actions = getResultActions(url(productIoId), HttpMethod.POST);
+
+        // then
+        OutboundConfirmResponse response = OutboundConfirmResponse.builder()
+            .productIoId(productIoId)
+            .productId(productId)
+            .quantity(productIoQuantity)
+            .build();
+        actions.andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value(ResponseStatus.OK))
+            .andExpect(jsonPath("$.message").value(ResponseMessage.OUTBOUND_CANCEL))
+            .andExpect(jsonPath("$.data", equalTo(asParsedJson(response))))
+            .andDo(print());
+
+        // productIo의 status가 변경됐는지 확인
+        ProductIo changedProductIo = productIoRepository.findById(productIoId)
+            .orElseThrow(ProductIoNotFoundException::new);
+        assertThat(changedProductIo.getStatus()).isEqualTo(ProductStatus.출고취소);
+      }
+    }
+
+    @Nested
+    @DisplayName("실패 케이스")
+    class FailCase {
+      @Test
+      @WithMockUser(roles = "ADMIN")
+      @DisplayName("productIo의 status가 출고대기 상태가 아니라면 400 BadRequest Error를 발생시킨다.")
+      void productIoStatusError() throws Exception {
+        // given
+        Category category = categoryRepository.save(
+            Category.builder().categoryName("카테고리").build());
+        Product product = productRepository.save(
+            Product.builder()
+                .productName(name)
+                .productNumber(number)
+                .codeNumber(codeNumber)
+                .companyName(companyName)
+                .stock(productQuantity)
+                .category(category)
+                .build());
+        productIoRepository.save(ProductIo.builder()
+            .status(ProductStatus.출고완료)
+            .quantity(productIoQuantity)
+            .product(product)
+            .build());
+
+        // when
+        ResultActions actions = getResultActions(url(productIoId), HttpMethod.POST);
+
+        // then
+        actions.andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.status").value(ExceptionStatus.CHANGE_OUTBOUND_STATUS))
+            .andDo(print());
+      }
+
+      @Test
+      @WithMockUser(roles = "ADMIN")
+      @DisplayName("productIo가 존재하지 않으면 403 Not Found Error를 발생시킨다.")
+      void productIoNotFound() throws Exception {
+        // given
+
+        // when
+        ResultActions actions = getResultActions(url(productIoId), HttpMethod.POST);
+
+        // then
+        actions.andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.status").value(ExceptionStatus.PRODUCT_IO_NOT_FOUND))
+            .andExpect(jsonPath("$.message").value(ExceptionMessage.PRODUCT_IO_NOT_FOUND));
+      }
+
+      @Test
+      @DisplayName("인증되지 않은 사용자가 접근하면 FORBIDDEN을 반환한다.")
+      void forbidden() throws Exception {
+        //given
+
+        //when
+        ResultActions actions = getResultActions(url(productId), HttpMethod.POST);
+
+        //then
+        actions.andExpect(status().isForbidden())
+            .andDo(print());
+      }
+    }
+  }
 }
