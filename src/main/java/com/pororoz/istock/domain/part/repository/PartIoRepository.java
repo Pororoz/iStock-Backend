3de790e2dd0718209773e07e9aba37c467f79f4b
Park HyeonJun <darkeroe@hanyang.ac.kr>
2023-03-24 23:16:35 +0900

    feat: partIo에 findByStatusContainingWithPart 추가

--- src/main/java/com/pororoz/istock/domain/part/repository/PartIoRepository.java
+++ src/main/java/com/pororoz/istock/domain/part/repository/PartIoRepository.java
@@ -3,6 +3,8 @@ package com.pororoz.istock.domain.part.repository;
 import com.pororoz.istock.domain.part.entity.PartIo;
 import com.pororoz.istock.domain.product.entity.ProductIo;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +15,11 @@ public interface PartIoRepository extends JpaRepository<PartIo, Long> {
       + "left join fetch pi.part "
       + "where pi.productIo = :productIo")
   List<PartIo> findByProductIoWithPart(@Param("productIo") ProductIo productIo);
-}
+
+  @Query(value = "select p from PartIo p "
+      + "left join fetch p.part "
+      + "where cast(p.status as string) like %:status%"
+      , countQuery = "select p from PartIo p "
+      + "where cast(p.status as string) like %:status%")
+  Page<PartIo> findByStatusContainingWithPart(@Param("status") String status,
+      Pageable pageable);}
