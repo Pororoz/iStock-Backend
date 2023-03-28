5b9a78f6c3da9d16e565ca317a33393231a360c5
Park HyeonJun <darkeroe@hanyang.ac.kr>
2023-03-28 15:44:55 +0900

    fix: findPartIoResponseSwagger, data 타입 변경

--- src/main/java/com/pororoz/istock/domain/part/swagger/response/FindPartIoResponseSwagger.java
+++ src/main/java/com/pororoz/istock/domain/part/swagger/response/FindPartIoResponseSwagger.java
@@ -1,8 +1,9 @@
 package com.pororoz.istock.domain.part.swagger.response;
 
+import com.pororoz.istock.common.dto.PageResponse;
 import com.pororoz.istock.common.utils.message.ResponseMessage;
 import com.pororoz.istock.common.utils.message.ResponseStatus;
-import com.pororoz.istock.domain.part.dto.response.PartResponse;
+import com.pororoz.istock.domain.part.dto.response.FindPartIoResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -15,5 +16,5 @@ public class FindPartIoResponseSwagger {
   @Schema(description = "Message", example = ResponseMessage.FIND_PART_IO)
   private String message;
 
-  private PartResponse data;
+  private PageResponse<FindPartIoResponse> data;
 }
