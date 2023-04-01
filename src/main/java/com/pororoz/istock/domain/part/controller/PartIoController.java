e9607eb539e19a3db72d37314742c0a6b3895f97
dohyeon-han <limited.hyeon@gmail.com>
2023-04-01 20:54:58 +0900

    feat: partIo 조회 uri에 part-id 쿼리 파라미터 추가

--- src/main/java/com/pororoz/istock/domain/part/controller/PartIoController.java
+++ src/main/java/com/pororoz/istock/domain/part/controller/PartIoController.java
@@ -48,8 +48,10 @@ public class PartIoController {
   public ResponseEntity<ResultDTO<PageResponse<FindPartIoResponse>>> findPartIO(
       @Parameter(hidden = true) Pageable pageable,
       @Schema(description = "부품 상태", example = "대기")
-      @RequestParam("status") String status) {
-    Page<FindPartIoResponse> partIoPage = partIoService.findPartIo(status, null, pageable)
+      @RequestParam("status") String status,
+      @Schema(description = "부품 id", example = "1")
+      @RequestParam("part-id") Long partId) {
+    Page<FindPartIoResponse> partIoPage = partIoService.findPartIo(status, partId, pageable)
         .map(FindPartIoServiceResponse::toResponse);
     PageResponse<FindPartIoResponse> response = new PageResponse<>(partIoPage);
     return ResponseEntity.ok(
